4672114788ef786fec00bf1d80e6e61f8fad4d7c
VHarisop <vharisop@gmail.com>
2023-07-25 15:49:48 -0500

    Add computation of Nystrom-factored inverse

--- operators/nystrom.py
+++ operators/nystrom.py
@@ -3,48 +3,82 @@ import numpy as np
 from numpy.random import choice
 import torch
 import torch.linalg as linalg
-from .operator import LinearOperator
+from .operator import LinearOperator, SelfAdjointLinearOperator
 
 from typing import Set, Tuple
+
+
+class NystromFactoredOperator(LinearOperator):
+    def __init__(self, factor: torch.Tensor):
+        super(NystromFactoredOperator, self).__init__()
+        self.factor = factor
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.factor @ x
+
+    def adjoint(self, x: torch.Tensor) -> torch.Tensor:
+        return self.factor.T @ x
+
+    def shape(self) -> torch.Size:
+        return self.factor.shape
+
+
+class NystromFactoredInverseOperator(SelfAdjointLinearOperator):
+    """A linear operator implementing the inverse of (s * I + V @ V.T)."""
+    U: torch.Tensor
+    scale_vec: torch.Tensor
+    shift: float
+
+    @torch.no_grad()
+    def __init__(self, operator: NystromFactoredOperator, shift: float):
+        super(SelfAdjointLinearOperator, self).__init__()
+        U, S, _ = torch.linalg.svd(operator.factor, full_matrices=False)
+        self.U = U
+        self.scale_vec = (S ** 2) / (S ** 2 + shift)
+        self.shift = shift
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return (1 / self.shift) * (
+            x - self.U @ (self.scale_vec * (self.U.T @ x))
+        )
 
 
 @torch.no_grad()
 def nystrom_approx_factored(
     operator: LinearOperator,
+    dim: int,
     rank: int,
     device: torch.device = torch.device("cpu"),
     dtype: torch.dtype = torch.float,
-) -> Tuple[LinearOperator, Set[int]]:
+) -> Tuple[NystromFactoredOperator, Set[int]]:
     """Compute the factored Nystrom approximation of a factored PSD linear operator.
 
     Args:
         operator: The Gram factor of the linear operator.
+        dim: The ambient dimension.
         rank: The target rank of the Nystrom approximation.
+        device: The Pytorch device (default: cpu).
+        dtype: The Pytorch dtype (default: float).
 
     Returns:
         The left factor of the Nystrom approximation.
     """
-    try:
-        m, d = operator.shape
-    except AttributeError:
-        raise ValueError("operator must have a .shape attribute!")
-
     # Multiplication with a canonical basis vector.
     def _mul_op_basis(idx: int) -> torch.Tensor:
-        basis_vec = torch.zeros(d, dtype=dtype, device=device)
+        basis_vec = torch.zeros(dim, dtype=dtype, device=device)
         basis_vec[idx] = 1
         return operator.forward(basis_vec)
 
-    sample_probs = torch.zeros(d, device=device, dtype=dtype)
+    sample_probs = torch.zeros(dim, device=device, dtype=dtype)
     # Create the vector of sample probabilities
-    for idx in range(d):
+    for idx in range(dim):
         sample_probs[idx] = torch.sum(_mul_op_basis(idx) ** 2)
     # Initialize Cholesky factor and list of pivots
-    factor = torch.zeros(d, rank, device=device, dtype=dtype)
+    factor = torch.zeros(dim, rank, device=device, dtype=dtype)
     pivots = set()
     for idx in range(rank):
         try:
-            sample_idx = choice(d, p=np.array(sample_probs / sum(sample_probs)))
+            sample_idx = choice(dim, p=np.array(sample_probs / sum(sample_probs)))
         except ValueError as err:
             logging.fatal(f"Problem with sampling: sample_probs={sample_probs}")
             # Re-throw ValueError
@@ -56,8 +90,4 @@ def nystrom_approx_factored(
         # Update sample probabilities and list of pivots
         sample_probs = torch.clamp_min(sample_probs - factor[:, idx] ** 2, 0.0)
         pivots.add(sample_idx)
-    factor_op = LinearOperator()
-    factor_op.forward = lambda x: factor @ x
-    factor_op.adjoint = lambda y: factor.T @ y
-    factor_op.shape = torch.Size((d, rank))
-    return factor_op, pivots
+    return NystromFactoredOperator(factor), pivots
